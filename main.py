2597859dfbb13a0ed0e0aeace78d41fcfad50680
Avtandil Kurbanov <avtandilkgg@gmail.com>
2020-01-20 00:32:54 +0300

    Bubble sort

---
+++ main.py
@@ -0,0 +1,17 @@
+old_list = [10,75,43,15,-4,27]
+new_list = old_list.copy()
+
+def bubble_sort(my_list):
+    last_item = len(my_list) - 1
+    for i in range(0, last_item):
+        for j in range(0, last_item - i):
+            if my_list[j] > my_list[j + 1]:
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+    return my_list
+
+bubble_sort(new_list)
+
+print("old_list ", old_list)
+print("new_list ", new_list)
+
+
