833650963a56a2a82483559a3e06c0ec7f76c6a4
Avtandil Kurbanov <avtandilkgg@gmail.com>
2020-01-23 20:55:27 +0300

    Chipher_Atbash

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-old_list = [10,75,43,15,-4,27]
-new_list = old_list.copy()
-
-def bubble_sort(my_list):
-    last_item = len(my_list) - 1
-    for i in range(0, last_item):
-        for j in range(0, last_item - i):
-            if my_list[j] > my_list[j + 1]:
-                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
-    return my_list
-
-bubble_sort(new_list)
-
-print("old_list ", old_list)
-print("new_list ", new_list)
+import copy
+alphabet = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+reversed_alphabet = copy.copy(alphabet)
+reversed_alphabet.reverse()
+def myFunction(text):
+    text = list(text)
+    for i in range(len(text)):
+        for j in range(len(alphabet)):
+            if text[i] == alphabet[j]:
+                text[i] = reversed_alphabet[j]
+                break
+    temp = "".join(text)
+    print(temp)
+myFunction('Автандил'.lower())
