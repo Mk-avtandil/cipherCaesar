0bae4f4476e99d1c85c6bdfbe8799f2a3738e5f0
Avtandil Kurbanov <avtandilkgg@gmail.com>
2020-01-24 23:57:46 +0300

    Cipher_Caesar

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-def binary_search(my_list, find, start, end):
-    if start > end:
-        return False
-    else:
-        mid = (start + end) // 2
-        if find == my_list[mid]:
-            return mid
-        elif find < my_list[mid]:
-            return binary_search(my_list, find, start, mid - 1)
-        else:
-            return binary_search(my_list, find, mid + 1, end)
+alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+encrypt = input("Enter your message: ")
+key = int(input("Please enter a key (number 1-26): "))
+encrypt = encrypt.lower()
+encrypted = ""
 
-my_list = [1, 2, 3, 4, 5, 7, 9, 11, 12, 15, 17, 19, 20, 25, 30, 50]
-find = 24
-x = binary_search(my_list, find, 0, len(my_list))
-if x == False:
-    print("Item ", find, "Not Found!")
-else:
-    print("Item ", find, "Found at index ", x)
+for letter in encrypt:
+    position = alphabet.find(letter)
+    new_position = position + key
+    if letter in alphabet:
+        encrypted += alphabet[new_position]
+    else:
+        encrypted += letter
+
+print(encrypted)
