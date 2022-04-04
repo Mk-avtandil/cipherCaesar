946da5587e39277c4b0fa1f000c0f1d4929a0a5a
avtandil <avtandilkgg@gmail.com>
2022-04-05 02:34:09 +0600

    Обновил старый код

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-encrypt = input("Enter your message: ")
-key = int(input("Please enter a key (number 1-26): "))
-encrypt = encrypt.lower()
-encrypted = ""
+from string import ascii_lowercase as eng_alphabet
 
-for letter in encrypt:
-    position = alphabet.find(letter)
-    new_position = position + key
-    if letter in alphabet:
-        encrypted += alphabet[new_position]
-    else:
-        encrypted += letter
 
-print(encrypted)
+class CipherCaesar:
+    def __init__(self, text, key):
+        if key > 26:
+            raise Exception('Значение ключа больше 26')
+        self.__text = text.lower()
+        self.__key = key
+        self.__alphabet = eng_alphabet * 2
+
+    def encript(self):
+        encrypted = ""
+        for letter in self.__text:
+            position = self.__alphabet.find(letter) + self.__key
+            if letter in self.__alphabet:
+                encrypted += self.__alphabet[position]
+            else:
+                encrypted += letter
+        return encrypted
+
+    def decrypt(self):
+        self.__text = self.encript()
+        decrypted = ""
+        for letter in self.__text:
+            position = self.__alphabet.find(letter) - self.__key
+            if letter in self.__alphabet:
+                decrypted += self.__alphabet[position]
+            else:
+                decrypted += letter
+        return decrypted
